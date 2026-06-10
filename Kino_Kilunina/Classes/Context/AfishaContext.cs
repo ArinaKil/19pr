929474b93c41e0917e77f8b29300bb9290ff443d
ArinaKil <arikakilka@mail.ru>
2026-06-10 18:52:23 +0500

    Создала контексты. Килунина

--- Kino_Kilunina/Classes/Context/AfishaContext.cs
+++ Kino_Kilunina/Classes/Context/AfishaContext.cs
@@ -1,12 +1,72 @@
-﻿using System;
+using Kino_Kilunina.Classes.Model;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kino_Kilunina.Classes.Context
 {
-    internal class AfishaContext
+    public class AfishaContext
     {
+        public List<Afisha> GetAll()
+        {
+            var list = new List<Afisha>();
+            var connection = Connection.OpenConnection();
+            var reader = Connection.Query("SELECT Id, IdKinoteatr, Name, Time, Price FROM Afisha", connection);
+            while (reader.Read())
+            {
+                list.Add(new Afisha(
+                    reader.GetInt32(0),
+                    reader.GetInt32(1),
+                    reader.GetString(2),
+                    reader.GetDateTime(3),
+                    reader.GetInt32(4)
+                ));
+            }
+            reader.Close();
+            Connection.CloseConnection(connection);
+            return list;
+        }
+
+        public Afisha GetById(int id)
+        {
+            Afisha afisha = null;
+            var connection = Connection.OpenConnection();
+            var reader = Connection.Query($"SELECT Id, IdKinoteatr, Name, Time, Price FROM Afisha WHERE Id = {id}", connection);
+            if (reader.Read())
+            {
+                afisha = new Afisha(
+                    reader.GetInt32(0),
+                    reader.GetInt32(1),
+                    reader.GetString(2),
+                    reader.GetDateTime(3),
+                    reader.GetInt32(4)
+                );
+            }
+            reader.Close();
+            Connection.CloseConnection(connection);
+            return afisha;
+        }
+
+        public void Add(Afisha afisha)
+        {
+            var connection = Connection.OpenConnection();
+            string sql = $"INSERT INTO Afisha (IdKinoteatr, Name, Time, Price) VALUES ({afisha.IdKinoteatr}, '{afisha.Name}', '{afisha.Time:yyyy-MM-dd HH:mm:ss}', {afisha.Price})";
+            new MySqlCommand(sql, connection).ExecuteNonQuery();
+            Connection.CloseConnection(connection);
+        }
+
+        public void Update(Afisha afisha)
+        {
+            var connection = Connection.OpenConnection();
+            string sql = $"UPDATE Afisha SET IdKinoteatr = {afisha.IdKinoteatr}, Name = '{afisha.Name}', Time = '{afisha.Time:yyyy-MM-dd HH:mm:ss}', Price = {afisha.Price} WHERE Id = {afisha.Id}";
+            new MySqlCommand(sql, connection).ExecuteNonQuery();
+            Connection.CloseConnection(connection);
+        }
+
+        public void Delete(int id)
+        {
+            var connection = Connection.OpenConnection();
+            new MySqlCommand($"DELETE FROM Afisha WHERE Id = {id}", connection).ExecuteNonQuery();
+            Connection.CloseConnection(connection);
+        }
     }
 }
--- Kino_Kilunina/Classes/Context/KinoContext.cs
+++ Kino_Kilunina/Classes/Context/KinoContext.cs
@@ -1,12 +1,70 @@
-﻿using System;
+using Kino_Kilunina.Classes.Model;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kino_Kilunina.Classes.Context
 {
-    internal class KinoContext
+    public class KinoContext
     {
+        public List<Kino> GetAll()
+        {
+            var list = new List<Kino>();
+            var connection = Connection.OpenConnection();
+            var reader = Connection.Query("SELECT Id, Name, CountZal, Count FROM Kino", connection);
+            while (reader.Read())
+            {
+                list.Add(new Kino(
+                    reader.GetInt32(0),
+                    reader.GetString(1),
+                    reader.GetInt32(2),
+                    reader.GetInt32(3)
+                ));
+            }
+            reader.Close();
+            Connection.CloseConnection(connection);
+            return list;
+        }
+
+        public Kino GetById(int id)
+        {
+            Kino kino = null;
+            var connection = Connection.OpenConnection();
+            var reader = Connection.Query($"SELECT Id, Name, CountZal, Count FROM Kino WHERE Id = {id}", connection);
+            if (reader.Read())
+            {
+                kino = new Kino(
+                    reader.GetInt32(0),
+                    reader.GetString(1),
+                    reader.GetInt32(2),
+                    reader.GetInt32(3)
+                );
+            }
+            reader.Close();
+            Connection.CloseConnection(connection);
+            return kino;
+        }
+
+        public void Add(Kino kino)
+        {
+            var connection = Connection.OpenConnection();
+            string sql = $"INSERT INTO Kino (Name, CountZal, Count) VALUES ('{kino.Name}', {kino.CountZal}, {kino.Count})";
+            new MySqlCommand(sql, connection).ExecuteNonQuery();
+            Connection.CloseConnection(connection);
+        }
+
+        public void Update(Kino kino)
+        {
+            var connection = Connection.OpenConnection();
+            string sql = $"UPDATE Kino SET Name = '{kino.Name}', CountZal = {kino.CountZal}, Count = {kino.Count} WHERE Id = {kino.Id}";
+            new MySqlCommand(sql, connection).ExecuteNonQuery();
+            Connection.CloseConnection(connection);
+        }
+
+        public void Delete(int id)
+        {
+            var connection = Connection.OpenConnection();
+            new MySqlCommand($"DELETE FROM Kino WHERE Id = {id}", connection).ExecuteNonQuery();
+            Connection.CloseConnection(connection);
+        }
     }
 }
