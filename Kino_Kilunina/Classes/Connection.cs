8561ddb49e62714d336396675530e807d818e3a4
ArinaKil <arikakilka@mail.ru>
2026-06-10 18:25:14 +0500

    Создала структуру, добавила изображения и создала класс подключения к БД. Килунина

---
+++ Kino_Kilunina/Classes/Connection.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino_Kilunina.Classes
+{
+    public class Connection
+    {
+        public static readonly string config = "server=127.0.0.1;uid=root;pwd=;database=Kino;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
